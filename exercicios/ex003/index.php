d8c9619cc4a6158484810a3a37fe1a118a197c3d
JefersonMatos9 <decoo.matos@live.com>
2024-04-24 16:24:29 -0300

    adcionando diretorios de exercicios

---
+++ exercicios/ex003/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos Primitivos em PHP</title>
+</head>
+<body>
+    <h1>
+        Teste de Tipos Primitivos</h1>
+        <?php
+        // 0x = hexadecimal 0b = binario 0 = Octal
+        // $num = 010;
+       // echo "O valor da variavel é $num";
+       
+     //$v = "Jeferson";
+      // var_dump($v);
+
+    //   $num = (int)3e2; // coerção 
+    //   var_dump($num);
+    //   echo  "O valor é $num";
+
+    //   $casado = true;
+    //   print "O valor para casado é $casado";
+
+//     $vet = [5,"Deco",3.5,4,7,false];
+// var_dump($vet);
+
+// class Pessoa {
+//     private String $nome;
+// }
+
+// $p = new Pessoa;
+
+
+// var_dump($p);
+
+// $nome = "Deco";
+// echo "Olá $nome \u{1F596}";
+// echo 'Olá $nome'
+
+// const ESTADO = "SP";
+// echo "Moro no Estado de:". ESTADO;
+
+$nom = "Deco";
+$snom = "Matos";
+
+echo "$nom \"Fodão\" $snom";
+
+        ?>
+</body>
+</html>
